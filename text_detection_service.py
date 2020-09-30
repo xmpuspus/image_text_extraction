a5f452722ccb3596d6b021395dc6eccf9855320f
Xavier Puspus <xavierpuspus@Xaviers-MacBook-Pro.local>
2020-09-30 09:46:15 +0800

    Added new text extraction code

--- text_detection_service.py
+++ text_detection_service.py
@@ -1,16 +1,15 @@
 
 from bentoml import BentoService, api, env, artifacts
 from bentoml.artifact import PickleArtifact
-from bentoml.adapters import ImageInput
+from bentoml.adapters import FileInput
 
 @artifacts([PickleArtifact('model')])
-@env(pip_dependencies=['pytesseract'], 
-     requirements_txt_file='pytesseract',
+@env(pip_dependencies=['easyocr'], 
      conda_channels=["conda-forge"], 
      conda_dependencies=["ruamel.yaml"])
 class TextDetectionService(BentoService):
     
-    @api(input=ImageInput())
+    @api(input=FileInput())
     def predict(self, image):
-        result = self.artifacts.model.detect_text(image)
+        result = self.artifacts.model.detect_text(image[0])
         return result
